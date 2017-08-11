10ac4a31ba717754ca934afd1179d61a006dbce6
Filip Ziętkowski <filip.zietkowski@gmail.com>
2017-08-11 21:47:24 +0200

    Created the NetBeans project structure.

---
+++ ExampleFoodBrowser/src/com/gmail/zietkowski/filip/examplefoodbrowser/ExampleFoodBrowser.java
@@ -0,0 +1,16 @@
+package com.gmail.zietkowski.filip.examplefoodbrowser;
+
+/**
+ * The main class.
+ * 
+ * @author Filip Ziętkowski
+ */
+public class ExampleFoodBrowser {
+
+    /**
+     * @param args The command line arguments.
+     */
+    public static void main(String[] args) {
+        ;
+    }
+}
