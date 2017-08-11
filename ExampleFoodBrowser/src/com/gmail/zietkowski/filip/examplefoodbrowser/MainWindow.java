e8c84e8ce68b1873b3fac7aeb516f7f5afd672d0
Filip Ziętkowski <filip.zietkowski@gmail.com>
2017-08-11 22:10:22 +0200

    Configured the main window properties.

--- ExampleFoodBrowser/src/com/gmail/zietkowski/filip/examplefoodbrowser/MainWindow.java
+++ ExampleFoodBrowser/src/com/gmail/zietkowski/filip/examplefoodbrowser/MainWindow.java
@@ -22,19 +22,22 @@ public class MainWindow extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Example Food Browser");
+        setBackground(java.awt.SystemColor.window);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 928, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 617, Short.MAX_VALUE)
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     /**
